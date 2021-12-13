78561f8593419114bdba6066a63c49be94237d0b
Tom Harrop <twharrop@gmail.com>
2021-12-13 14:39:39 +1100

    chmod argh

--- post_install.sh
+++ post_install.sh
@@ -2,3 +2,14 @@
 
 git clone https://github.com/owntone/owntone-server.git \
 	/owntone-build
+
+exit 0
+
+cd /owntone-build || exit 1
+
+pw adduser \
+	owntone \
+	-d /nonexistent \
+	-s /usr/sbin/nologin \
+	-c "owntone user"
+
