59294b74abd7b0d2596cb8ab31fbabe2c5b4e46e
Tom Harrop <twharrop@gmail.com>
2023-11-29 19:30:09 +1100

    Update post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -5,7 +5,7 @@
 export CLASSPATH=$CLASSPATH:/usr/local/share/java:/usr/local/share/java/classes/antlr-3.5.2-complete.jar
 export CFLAGS="-march=native -g -I/usr/local/include -I/usr/include"
 export LDFLAGS="-L/usr/local/lib -L/usr/lib"
-# export OWNTONE_VERSION="28.3"
+export OWNTONE_VERSION="28.8"
 
 # add owntone user
 pw adduser \
@@ -17,17 +17,17 @@ pw adduser \
 # download owntone
 git clone https://github.com/owntone/owntone-server /owntone-build
 
-# mkdir /owntone-build
-# wget \
-# 	-O /owntone.tar.xz \
-# 	"https://github.com/owntone/owntone-server/releases/download/${OWNTONE_VERSION}/owntone-${OWNTONE_VERSION}.tar.xz"
+mkdir /owntone-build
+wget \
+	-O /owntone.tar.xz \
+	"https://github.com/owntone/owntone-server/releases/download/${OWNTONE_VERSION}/owntone-${OWNTONE_VERSION}.tar.xz"
 
-# tar -Jxf /owntone.tar.xz \
-# 	-C /owntone-build \
-# 	--strip-components 1
+tar -Jxf /owntone.tar.xz \
+	-C /owntone-build \
+	--strip-components 1
 
 cd /owntone-build || exit 1
-git checkout 5efe0ee
+# git checkout 5efe0ee
 
 # build owntone
 autoreconf -vi
@@ -40,7 +40,7 @@ gmake install
 
 # get the startup script from github
 wget -O /usr/local/etc/rc.d/owntone \
-	"https://raw.githubusercontent.com/owntone/owntone-server/28.8/scripts/freebsd_start.sh"
+	"https://raw.githubusercontent.com/owntone/owntone-server/${OWNTONE_VERSION}/scripts/freebsd_start.sh"
 chmod 755 /usr/local/etc/rc.d/owntone
 
 # set permissions
@@ -70,7 +70,7 @@ chown -R owntone:owntone /srv/music
 # tidy up
 cd / || exit 1
 rm -r /owntone-build
-# rm /owntone.tar.xz
+rm /owntone.tar.xz
 rm -r /mpc-build
 rm /mpc.tar.gz
 
